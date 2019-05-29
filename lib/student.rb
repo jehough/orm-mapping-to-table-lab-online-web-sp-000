53caf28f717e9c9adf7c90cda14c6e0c9331d33a
Jim Hough <teachlife@outlook.com>
2019-05-29 01:05:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,5 +18,9 @@ class Student
   end
       # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  def self.drop_table
+    sql = "DROP TABLE students;"
+    DB[:conn].execute(sql)
+  end
 
 end
